7caa009a20e16d470e20256ee6873a7c51cf5281
hspark90 <hspark90@i-fam.net>
2020-11-19 20:20:24 +0900

    Update kran.R

--- kran.R
+++ kran.R
@@ -1,3 +1,3 @@
 rprofile_path = file.path(Sys.getenv("HOME"), ".Rprofile")
-write('\noptions(repos=c(getOption(\'repos\'), CRAN=\'https://cloud.r-project.org\', KRAN=\'http://kict-r.prj.kr\'))\n', rprofile_path, append =  TRUE)
+write('\noptions(repos=c(getOption(\'repos\'), CRAN=\'https://cloud.r-project.org\', KRAN=\'https://kict-r.prj.kr\'))\n', rprofile_path, append =  TRUE)
 cat('Your Rprofile has been updated to include KRAN. Please restart R for changes to take effect.')
